59c41828b7c0d5df8823c45f844fda20367d2186
danielgiron <73446404+danielgiron@users.noreply.github.com>
2023-07-13 23:42:09 -0400

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
-// const session = require("cookie-session");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = OpenAI;
+const configuration = new Configuration({
+  organization: process.env.OPENAI_API_ORG,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,5 +24,28 @@ app.listen(PORT, () => {
 });
 
 app.get("/home", (req, res) => {
+  console.log("Someone connected to /home");
   res.send("Connected to /home");
+});
+
+app.post("/gpt", async (req, res) => {
+  const { message } = req.body;
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    max_tokens: 100,
+    temperature: 1.4,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+    messages: [
+      {
+        role: "system",
+        content: `${process.env.GAME_DETAILS}`,
+      },
+      { role: "user", content: `${message}` },
+    ],
+  });
+  const text = completion?.data.choices[0].message;
+  console.log(text);
+  res.send(text);
 });
